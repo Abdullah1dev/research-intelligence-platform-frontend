b3a5d02812a66cee7f64b8c7b851073e9c4be3aa
Abdullah Tahir <242677@students.au.edu.pk>
2026-09-15 00:13:30 +0500

    Initilaze frontend files and download frontend depenendcies

---
+++ src/router/index.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Research Intelligence Platform</div>,
+  },
+]);
+
+export default router;
